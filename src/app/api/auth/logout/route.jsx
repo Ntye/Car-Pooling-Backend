ff2cf76cf3bb00ab5e84faf14197535ff85b58fd
Ntye <ntye.nina@gmail.com>
2024-06-14 05:07:02 +0100

    authentification complete

---
+++ src/app/api/auth/logout/route.jsx
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+
+export async function POST(request) {
+
+	return NextResponse.json(
+		{
+			message: "Logout successful",
+			success: true
+		}
+	);
+}
