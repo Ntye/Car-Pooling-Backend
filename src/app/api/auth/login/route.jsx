11f1bbadc0b790910a0e9186ec23586456580cbc
Ntye <ntye.nina@gmail.com>
2024-06-14 04:28:37 +0100

    initial commit

---
+++ src/app/api/auth/login/route.jsx
@@ -0,0 +1,44 @@
+import { connect } from '@/dbConfiguration/dbConfiguration';
+import User from '@/models/userModel';
+import { NextResponse } from 'next/server';
+import bcryptjs from 'bcryptjs';
+
+connect();
+
+export async function POST(request) {
+	try {
+		const reqBody = await request.json();
+		const { email, password } = reqBody;
+
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			return NextResponse.json(
+				{ error: "Invalid email or password" },
+				{ status: 401 }
+			);
+		}
+
+		const isMatch = await bcryptjs.compare(password, user.password);
+		if (!isMatch) {
+			return NextResponse.json(
+				{ error: "Invalid email or password" },
+				{ status: 401 }
+			);
+		}
+
+		return NextResponse.json(
+			{
+				message: "Login successful",
+				success: true,
+				token,
+				user
+			}
+		);
+	} catch (error) {
+		return NextResponse.json(
+			{ error: error.message },
+			{ status: 500 }
+		);
+	}
+}
